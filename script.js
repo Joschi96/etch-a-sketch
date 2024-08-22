8ba55115faec7ad97225a2413a7f4117882b8e65
Joschi96 <117238956+Joschi96@users.noreply.github.com>
2024-08-22 17:25:45 +0200

    added button to change number of pixels per length

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-document.body.onload = addGrid;
-
 let size = 16; //Number of squares in one row/column
-let boxSize = 10; // Size of each square in pixels
+let containerWidth = 400;
+let boxSize = (containerWidth/size); // Size of each square in pixels
+
+document.body.onload = addGrid(size);
 
 let container = document.querySelector(".container");
-container.style.maxWidth = size * boxSize+'px'; // Dynamically limiting canvas width to size*boxSize
+container.style.maxWidth = containerWidth+'px'; // Dynamically limiting canvas width to size*boxSize
+container.style.minWidth = containerWidth+'px';
+
+let btn = document.querySelector("button");
 
 // Function for adding the initial 16x16 grid
-function addGrid(){
-    for (let i = 0; i < size*size; i++) {
+function addGrid(length){
+    for (let i = 0; i < length*length; i++) {
         let element = document.createElement('div');
         element.classList.add("gridElement");
         element.style.display = "flex";
@@ -23,3 +27,13 @@ function addGrid(){
     }
 }
 
+btn.addEventListener("click", function changeGrid() {
+    let input = prompt("Enter the number of pixels per side!\nMin 1, Max 100");
+    size = input;
+    boxSize = (containerWidth/size);
+    let oldGrid = document.querySelectorAll('.gridElement');
+    oldGrid.forEach(element => {
+        element.remove();
+    });
+    addGrid(size);
+});
