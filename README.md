2271cadbbc41755668936ca15da043d3e83587ca
Joschi96 <117238956+Joschi96@users.noreply.github.com>
2024-08-22 17:52:55 +0200

    improved perdormance on adding and delting grid; Added rainbow-function

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 This will be a browser version of something between a sketchpad and an Etch-A-Sketch.
 
-Initially 16x16 "pixels" wide - One "pixel" has an adaptable real px size.
+Initially 16x16 "pixels" wide canvas.
 
 The canvas squares/"pixels" are created as flex-boxes and NOT set up as html grid for learning reasons.
+
+Button on top can be used to change the amount of "pixels" per side.
 
 Work in Progress
--- script.js
+++ script.js
@@ -12,28 +12,44 @@ let btn = document.querySelector("button");
 
 // Function for adding the initial 16x16 grid
 function addGrid(length){
+    // Create a DocumentFragment to hold the new grid elements
+    let fragment = document.createDocumentFragment();
+    let container = document.querySelector('.container');
+
     for (let i = 0; i < length*length; i++) {
         let element = document.createElement('div');
         element.classList.add("gridElement");
         element.style.display = "flex";
         element.style.width = boxSize+'px';
         element.style.height = boxSize+'px';
-        document.querySelector('.container').appendChild(element);
         
         // Adding "hover" effect directly after creating each element
-        element.addEventListener("mouseenter", function() {
-            this.style.backgroundColor = "red";
+        element.addEventListener("mouseenter", function() {  
+            let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
+            this.style.backgroundColor = randomColor;
         });
+
+        // Append the new element to the fragment
+        fragment.appendChild(element);
     }
+
+    // Append the entire fragment to the container at once
+    container.appendChild(fragment);
 }
 
 btn.addEventListener("click", function changeGrid() {
-    let input = prompt("Enter the number of pixels per side!\nMin 1, Max 100");
+    let input = prompt("Enter the number of pixels per side!\nMin 1, Max 100");    
+    if (input < 1 || input > 100){
+        alert("Please enter a valid number between 1 and 100.");
+        return;
+    }
+
     size = input;
     boxSize = (containerWidth/size);
-    let oldGrid = document.querySelectorAll('.gridElement');
-    oldGrid.forEach(element => {
-        element.remove();
-    });
+
+    // Clear the container
+    container.innerHTML = '';
+
+    // Rebuild  the grid with the new size
     addGrid(size);
 });
